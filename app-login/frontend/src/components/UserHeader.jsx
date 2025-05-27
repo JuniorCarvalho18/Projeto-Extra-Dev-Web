b1be4599bd5a7103b34c47b8107b90f23f6fccf0
Junior Carvalho <laerciojrcarvalho18@outlook.com>
2025-05-27 11:19:08 -0400

    hummm

--- app-login/frontend/src/components/UserHeader.jsx
+++ app-login/frontend/src/components/UserHeader.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 
+function CloseSidebar() {
+  const Sidebar = document.querySelector('.sidebar');
+
+  if (Sidebar) {
+    if (Sidebar.style.display === 'flex' || Sidebar.style.display === '') {
+      Sidebar.style.display = 'none';
+    } else {
+      Sidebar.style.display = 'flex';
+    }
+    Sidebar.style.transition = '0.5s';
+  }
+}
+
 function UserHeader({ user }) {
   return (
     <div className="user-header">
+      <i id="burguer" className="material-icons"  onClick={CloseSidebar}>menu</i>
       <img 
         src={`http://localhost:3001/uploads/${user.photo}`} 
         alt="Foto" 
         className="user-photo" 
-    />
-
+      />
       <div>
         <h4>{user.fullName}</h4>
         <p>{user.role}</p>
